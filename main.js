ac1225d61f132b02e63015820ed3577e697be195
stereoslogos <bigztar@gmail.com>
2021-04-03 11:33:50 -0500

    first-commit

---
+++ main.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards',{delay: 500});
+ScrollReveal().reveal('.cards-banner',{delay: 500});
+
+console.log("στέρεο λóγος web &copy; 2021");
